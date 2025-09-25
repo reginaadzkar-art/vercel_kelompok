3eb06e9c4ced96bf863fbebad9924db7885ed198
Regina Afriliani <reginaadzkar@gmail.com>
2025-09-25 14:02:01 +0700

    mengubah asset

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Biodata 5 Orang</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="{{ asset('style.css') }}">
 </head>
 <body>
   <h1>Daftar Biodata</h1>
